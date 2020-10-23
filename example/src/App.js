3b002d35c5798653cbdafd911d2a5a834a74373c
Winnie Xiao <winniex@stanford.edu>
2020-10-23 15:43:54 +0200

    Added closing button and hover

--- example/src/App.js
+++ example/src/App.js
@@ -1,34 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import BrowserFrame from "react-browser-frame";
 const iframeSrc =
   "https://codepen.io/liamj/embed/preview/VdqgZK?height=500&amp;theme-id=39032&amp;default-tab=result";
-export default () => (
-  <div style={{ padding: "30px", maxWidth: "800px" }}>
-    <BrowserFrame url="http://zombo.com">
-      <img
-        src="https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg"
-        alt="A handsome doggo"
-        style={{ width: "100%" }}
-      />
-    </BrowserFrame>
-    <br />
-    <br />
-    <BrowserFrame url="https://example.biz" padding="30px 20px">
-      <p>It was the best of times, it was the blurst of times...</p>
-    </BrowserFrame>
-    <br />
-    <br />
-    <BrowserFrame>
-      <iframe
-        width="100%"
-        height="500px"
-        scrolling="no"
-        title="Breezy Windows logo"
-        src={iframeSrc}
-        frameBorder="no"
-        allowtransparency="true"
-        allowFullScreen=""
-      />
-    </BrowserFrame>
-  </div>
-);
+
+function App() {
+  const [vpn, setVpn] = useState(true);
+
+  return (
+    <div style={{ padding: "30px", maxWidth: "800px" }}>
+      <BrowserFrame url="http://zombo.com" visible={vpn} setVisible={setVpn}>
+        <img
+          src="https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg"
+          alt="A handsome doggo"
+          style={{ width: "100%" }}
+        />
+      </BrowserFrame>
+      <br />
+      <br />
+      <BrowserFrame url="https://example.biz" padding="30px 20px">
+        <p>It was the best of times, it was the blurst of times...</p>
+      </BrowserFrame>
+      <br />
+      <br />
+      <BrowserFrame>
+        <iframe
+          width="100%"
+          height="500px"
+          scrolling="no"
+          title="Breezy Windows logo"
+          src={iframeSrc}
+          frameBorder="no"
+          allowtransparency="true"
+          allowFullScreen=""
+        />
+      </BrowserFrame>
+    </div>
+  );
+}
+
+export default App;
