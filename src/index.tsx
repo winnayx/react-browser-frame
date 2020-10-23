66dfb013a418c48bd90c387c985c3f06aa5a83a5
Winnie Xiao <winniex@stanford.edu>
2020-10-23 15:44:16 +0200

    Updated hover and closing

--- src/index.tsx
+++ src/index.tsx
@@ -4,31 +4,40 @@ import "./styles.css";
 export type Props = {
   url?: string;
   padding?: string;
+  visible: boolean;
+  setVisible: Function;
   children: React.ReactNode;
 };
 
-export default ({ url, padding, children }: Props) => (
-  <div className="browser-frame">
-    <div className="browser-frame__top">
-      <div className="browser-frame__button browser-frame__button--red" />
-      <div className="browser-frame__button browser-frame__button--yellow" />
-      <div className="browser-frame__button browser-frame__button--green" />
-      {url && (
-        <div className="browser-frame__address" aria-hidden>
-          {url}
+export default ({ url, padding, visible, setVisible, children }: Props) => (
+  <div>
+    {visible && (
+      <div className="browser-frame">
+        <div className="browser-frame__top">
+          <div
+            className="browser-frame__button browser-frame__button--red"
+            onClick={() => setVisible(false)}
+          />
+          <div className="browser-frame__button browser-frame__button--yellow" />
+          <div className="browser-frame__button browser-frame__button--green" />
+          {url && (
+            <div className="browser-frame__address" aria-hidden>
+              {url}
+            </div>
+          )}
+          <div className="browser-frame__burger">
+            <span className="browser-frame__burger-line" />
+            <span className="browser-frame__burger-line" />
+            <span className="browser-frame__burger-line" />
+          </div>
         </div>
-      )}
-      <div className="browser-frame__burger">
-        <span className="browser-frame__burger-line" />
-        <span className="browser-frame__burger-line" />
-        <span className="browser-frame__burger-line" />
+        <div
+          className="browser-frame__body"
+          style={{ padding: padding ? padding : "" }}
+        >
+          {children}
+        </div>
       </div>
-    </div>
-    <div
-      className="browser-frame__body"
-      style={{ padding: padding ? padding : "" }}
-    >
-      {children}
-    </div>
+    )}
   </div>
 );
